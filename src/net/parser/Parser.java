e2980f484ce5af0ebf91e748ec858f2079993c51
capacitypp <aoi0x4c4f5645@gmail.com>
2015-05-03 11:00:39 +0900

    トークンを管理する基本的なTokenクラスを作成．

--- src/net/parser/Parser.java
+++ src/net/parser/Parser.java
@@ -2,8 +2,17 @@ package net.parser;
 
 public class Parser {
 
+	/**
+	 *
+	 * @param numericalFormula : ”®‚Ì•¶š—ñD"a=b+c"‚È‚ÇD
+	 */
+	public Parser(String numericalFormula) {
+		System.out.println(numericalFormula);
+	}
+
 	public static void main(String[] args) {
 		System.out.println("hello, world!");
+		new Parser("a=b+c");
 	}
 
 }
---
+++ src/net/token/Token.java
@@ -0,0 +1,28 @@
+package net.token;
+
+public class Token {
+	public final int TYPE_VARIABLE = 1;		//変数
+	public final int TYPE_OPERATOR = 2;		//演算子
+	public final int TYPE_NUMBER = 3;		//数値
+	public final int TYPE_SEMICOLON = 4;	//セミコロン
+
+	private String token;	//数式中の文字列(トークン)
+	private int type;		//トークン型
+	public Token(String token, int type) {
+		this.token = token;
+		this.type = type;
+	}
+	//setter and getter
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+}
